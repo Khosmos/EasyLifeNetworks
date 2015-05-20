edead954804788c8d53c5ee87106b46cf80609f8
Cosme Corrêa <cosmefc@id.uff.br>
2015-05-20 00:10:33 -0400

    come cleaning

--- modules/NetDot.sh
+++ modules/NetDot.sh
@@ -117,7 +117,6 @@ case $OSVERSION in
 		wget http://netdot.uoregon.edu/pub/dists/netdot-1.0.7.tar.gz
 		tar xzvf netdot-1.0.7.tar.gz -C /usr/local/src/
 		mv /usr/local/src/netdot-1.0.7/ /usr/local/src/netdot/
-		read
 		
 		#3) Download Netdisco mibs
 		wget http://ufpr.dl.sourceforge.net/project/netdisco/netdisco-mibs/1.5/netdisco-mibs-1.5.tar.gz
@@ -126,7 +125,6 @@ case $OSVERSION in
 		mv netdisco-mibs-1.5/ /usr/share/netdisco/mibs
 		cp -p $ModDir/NetDot/snmp.conf /etc/snmp/
 		service snmpd restart
-		read
 		
 		yum install perl perl-CPAN -y
 		cd /tmp
@@ -135,73 +133,25 @@ case $OSVERSION in
 		yum install httpd httpd-devel mod_perl mod_perl-devel perl-LDAP rrdtool gcc -y
 		cd /usr/local/src/netdot/
 		( echo $NETDOTDB; sleep 5; echo y ) | make rpm-install
-		read
 		#yum install perl-Net-Appliance-Session perl-Net-Patricia -y
 		
-		read
 		yum install graphviz-devel graphviz-gd libapreq2-devel libpng-devel openssl-devel -y
 		( echo $NETDOTDB; echo yes ) | make installdeps
-		read
 		
 	#7 Configure Database Settings for Netdot
 	SetUpDB
 
 	#8 Install Netdot
 	InstallNetdot
+	
+	#8 SetUp Apache	
+	cp /usr/local/netdot/etc/netdot_apache2_ldap.conf /etc/httpd/conf.d/netdot.conf
+	service httpd restart
+	
+	#9 SetUp Cron	
+	cp /usr/local/src/netdot/netdot.cron /etc/cron.d/netdot
+
 	read
-		
-		
-		
-#		yum install httpd httpd-devel mod_perl mod_perl-devel perl-LDAP rrdtool gcc -y
-		# DB-MYSQL or
-		#yum install mysql-client mysql-server -y
-		#DB-PG
-#		yum install postgresql-server postgresql perl-DBD-Pg -y
-		
-#		yum install graphviz make  perl-CPAN  -y
-#		yum install perl-CGI perl-Digest-SHA1 perl-GraphViz perl-Log-Dispatch perl-Log-Log4perl perl-Module-Build  perl-Net-DNS perl-Parallel-ForkManager perl-SNMP-Info perl-Socket6 perl-XML-Simple perl-Module-Build perl-Class-DBÂ  perl-Class-DBI-AbstractSearch perl-libapreq2 perl-HTML-Mason perl-Apache-Session perl-SQL-Translator perl-NetAddr-IP perl-Net-Patricia perl-Net-Appliance-Session  perl-Carp-Assert -y
-		#yum install  perl-NetAddr-IP
-#		yum install graphviz-devel graphviz-gd libapreq2-devel libpng-devel openssl-devel -y
-#		cd /usr/local/src/netdot-1.0.7/
-#		echo Pg | make installdeps
-		#wget http://pkgs.repoforge.org/perl-Net-DNS-ZoneFile-Fast/perl-Net-DNS-ZoneFile-Fast-1.12-1.el6.rf.noarch.rpm
-		#yum localinstall perl-Net-DNS-ZoneFile-Fast-1.12-1.el6.rf.noarch.rpm -y
-		#For Mysql
-		#service mysqld start
-		#mysql_secure_installation
-		##edit my.cnf
-		#[mysqld]
-		#max_allowed_packet = 16M
-		#For Postgresql
-		#service postgresql initdb
-		#sed -i 's/host    all         all         127.0.0.1\/32          ident/host    all         all         127.0.0.1\/32          md5/g' /var/lib/pgsql/data/pg_hba.conf
-		#service postgresql start
-		#sudo -u postgres psql postgres
-		#\password postgres
-		#123456
-		#^D
-		##echo -e "$POSTGRESPASSWD\n$POSTGRESPASSWD" | passwd postgres
-		#echo -e "123456\n123456" | passwd postgres
-		##su - postgres -c "psql postgres -c \"ALTER USER postgres WITH PASSWORD '$POSTGRESPASSWD'\""
-		#su - postgres -c "psql postgres -c \"ALTER USER postgres WITH PASSWORD '123456'\""
-		#cp etc/Default.conf etc/Site.conf
-		#vim etc/Site.conf
-		#	Pay attention here
-		#	Change NETDOTNAME
-		#	Change DB_TYPE		mysql|Pg
-		#	Change DB_DBA
-		#		mysql	->	root
-		#		Pg	->	postgres
-		#	Change DB_DBA_PASSWORD
-		#	Change DB_PORT, if Pg to 5432
-		#	Change DB_NETDOT_PASS	123456
-		#make rpm-install
-		#make installdb
-		#make install
-		#cd /usr/local/netdot/
-		#vi /usr/local/netdot/etc/netdot_apache2_ldap.conf
-		#cp /usr/local/netdot/etc/netdot_apache2_ldap.conf /etc/httpd/conf.d/netdot.conf
-		#service httpd restart
-		#cp /usr/local/src/netdot-1.0.7/netdot.cron /etc/cron.d/netdot
+
 		;;
 esac
