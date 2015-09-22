5e4dc3ca802aaad357b4262d96a7b613c6e2ea54
AnaSilverio <anasilverio@id.uff.br>
2015-09-22 00:02:53 -0300

    template changed and comments added

--- modules/PostgreSQLUp.sh
+++ modules/PostgreSQLUp.sh
@@ -2,35 +2,31 @@
 # Easy Life for Networks
 #
 # Configuration Tool for an Easy Life
-# Version 20150520
+# Version 20150921
 #
 # PostgreSQL module
 #
 # Cosme Faria Corrêa
 # helgadb 
-# 
+# Ana Carolina Silvério
 # ...
 #
 #set -xv
 
 clear
+DisplayYN "EasyLife Networks - PostgreSQLUp " \
+"This module will :
+1) Install PostgreSQL PGDG v9.2 Repository
+2) Install PostgreSQL v9.2
+3) Install pgAdmin3 v1.16
+4) Initialize PostgreSQL
+5) Configure password for user postgres
+6) Configure pg_hba.conf
 
-cat <<-EOF
-=========================================
-| Easy Life for Networks |
-=========================================
-PostgreSQL Module
+$TAIL" "Install" "Cancel" || exit
 
-This module will:
-*) Install PostgreSQL PGDG v9.2 Repository
-*) Install PostgreSQL v9.2
-*) Install pgAdmin3 v1.16
-*) Initialize PostgreSQL
-*) Configure password for user postgres
-*) Configure pg_hba.conf
 
-Press <Enter> to continue
-EOF
+
 
 if [ $OSVERSION = "7" ]; then
     DisplayError "Unable to install" "Sorry, this module is only for CentOS 6"
@@ -39,7 +35,7 @@ fi
 
 read
 
-# a) Install PostgreSQL PGDG v9.2 Repository
+# 1) Install PostgreSQL PGDG v9.2 Repository
 centosRepoPath="/etc/yum.repos.d/CentOS-Base.repo"
 centosRepoPathOld="/etc/yum.repos.d/CentOS-Base.repo.$(date +%Y%m%d-%H%M%S)"
 cp $centosRepoPath $centosRepoPathOld
@@ -61,25 +57,25 @@ if [ -f $ModDir'PostgreSQL/'$pgdgRpm ];
 fi
 rpm -ivh $ModDir'PostgreSQL/'$pgdgRpm
 
-# b) Install PostgreSQL v9.2
+# 2) Install PostgreSQL v9.2 - why we need to build this version?
 postgre="postgresql92-server.x86_64"
 yum -y install $postgre
 
-# c) Install pgAdmin3 v1.16
+# 3) Install pgAdmin3 v1.16 - why we need to build this version?
 pgadmin="pgadmin3_92.x86_64"
 yum -y install $pgadmin
 sed -i 's/Exec=\/usr\/bin\/pgadmin3_92/Exec=\/usr\/bin\/pgadmin3/g' /usr/share/applications/fedora-pgadmin3_92.desktop
 
-# d) Initialize PostgreSQL
+# 4) Initialize PostgreSQL
 service postgresql-9.2 initdb
 chkconfig postgresql-9.2 on
 service postgresql-9.2 restart
 
-# e) Configure password for user postgres
+# 5) Configure password for user postgres
 echo -e "$POSTGRESPASSWD\n$POSTGRESPASSWD" | passwd postgres
 su - postgres -c "psql postgres -c \"ALTER USER postgres WITH PASSWORD '$POSTGRESPASSWD'\""
 
-# f) Configure pg_hba.conf
+# 6) Configure pg_hba.conf
 su - postgres -c "cp /var/lib/pgsql/9.2/data/pg_hba.conf /var/lib/pgsql/9.2/data/pg_hba.conf.old.$(date +%Y%m%d-%H%M%S)"
 sed -i 's/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            md5/g' /var/lib/pgsql/9.2/data/pg_hba.conf
 sed -i 's/host    all             all             ::1\/128                 ident/host    all             all             ::1\/128                 md5/g' /var/lib/pgsql/9.2/data/pg_hba.conf
