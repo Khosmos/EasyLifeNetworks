60476786b764f97febdd5a231bbad3c1e1f92edc
AnaSilverio <anasilverio@id.uff.br>
2015-10-05 12:48:47 -0300

    Translating the script

--- scripts/fazbkp.sh
+++ scripts/fazbkp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash                           
 # versão 20090605                     
-# sistema de bkp que executa as seguintes tarefas
-#  - gera um ldif do LDAP                        
-#  - envia email com log de erros                
+# bkp system that performs the following tasks
+#  - creates an LDAP ldif                        
+#  - sends error log with email                
 
-# Chamar pelo cron com o seguinte formato:
+# Calling using cron in the following format:
 # /root/fazbkp.sh >> /var/log/backup-ok.log 2> /var/log/backup-erro.log
 
 # Cosme Corrêa - cosmefc@id.uff.br
 
-#  Descomente para debug
+#  Uncomment to debug
 set -xv                 
 
 CLIENTE=`hostname -s`
@@ -20,19 +20,20 @@ DIAM=`date +%d`
 DESTINATARIO="suporte@uff.br"
 MENSAGEM="/var/log/backup-erro.log"    
 
-# Atividade 1 - LDAP
+# Activity 1 - LDAP
 echo Atividade 1 - LDAP
 mkdir -p $BKPHOME/LDAP
 /usr/sbin/slapcat -l $BKPHOME/LDAP/LDAP-$CLIENTE-$INICIO.ldif
 FIM=`date +%Y%m%d-%H%M%S`                                    
 
-# Atividade 2 - Transfere arquivo para o secundário
-echo  Atividade 2 - Transfere arquivo para o secundário
+
+# Activity 2 - Transfers file to the secondary
+echo  Activity 2 - Transfers file to the secondary
 # /usr/bin/scp  $BKPHOME/LDAP/LDAP-$CLIENTE-$INICIO.ldif root@xldap2.uff.br:/home/LDAP
 FIM=`date +%Y%m%d-%H%M%S`                                    
 
-# Atividade 3 - Envia e-mail com log de erros
-echo  Atividade 3 - Envia e-mail com log de erros
+# Activity 3 - Send email with error log
+echo  Activity 3 - Send email with error log
 ASSUNTO="Backup - "$CLIENTE" - "$INICIO
 #/bin/mail -s "$ASSUNTO" "$DESTINATARIO" < $MENSAGEM
 FIM=`date +%Y%m%d-%H%M%S`                                    
