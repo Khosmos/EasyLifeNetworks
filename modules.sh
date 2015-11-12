5db94a236808be6e69e69964b991d58625e5cd2b
Cosme Corrêa <cosmefc@id.uff.br>
2015-11-12 16:21:57 -0500

    Fixing issue #18

--- modules.sh
+++ modules.sh
@@ -14,7 +14,7 @@
 CurDir='/usr/share/EasyLifeNetworks'
 CFGFile=$CurDir'/confs/variables.sh'
 Start=`date +%Y%m%d-%H%M%S`
-Steps='01 Variables 02 Network 03 SNMPD 04 Logs 05 LDAP 06 LDAPolc 07 phpLDAPadmin 08 SSHD 09 NX 10 DenyHosts 11 NTPD 12 Apache 13 Monitorix 14 DNSMasq 15 MRTG 16 Nagios 17 RADIUS 18 RadSecProxy 19 Firewall 20 Conntrack 21 Postfix 22 PostgreSQL 23 PostgreSQLUp 24 JavaJDK 25 NetDot 26 JavaOracle 27 JBossAS 28 APImageGenerator 29 Exit'
+Steps='01 Variables 02 Network 03 SNMPD 04 Logs 05 Apache 06 LDAP 07 LDAPolc 08 phpLDAPadmin 09 SSHD 10 NX 11 DenyHosts 12 NTPD 13 Monitorix 14 DNSMasq 15 MRTG 16 Nagios 17 RADIUS 18 RadSecProxy 19 Firewall 20 Conntrack 21 Postfix 22 PostgreSQL 23 PostgreSQLUp 24 JavaJDK 25 NetDot 26 JavaOracle 27 JBossAS 28 APImageGenerator 29 Exit'
 #    	Steps='Variables Network SNMPD Logs LDAP LDAPolc phpLDAPadmin SSHD NX DenyHosts NTPD Apache Monitorix DNSMasq MRTG Nagios RADIUS RadSecProxy Firewall Conntrack Postfix PostgreSQL PostgreSQLUp JavaJDK NetDot JavaOracle JBossAS Exit' 
 OPTIONS=$(SelectMenu "Easy Life Networks" "Module Selection" $Steps) 
 exitstatus=$?
@@ -33,28 +33,73 @@ if [ $exitstatus = 0 ]; then
 	    source $ModDir'Logs.sh'
 	    ;;
 	05 )
-	    source $ModDir'LDAP.sh'
+	    source $ModDir'Apache.sh'
 	    ;;
 	06 )
-	    source $ModDir'LDAPolc.sh'
+	    source $ModDir'LDAP.sh'
 	    ;;
 	07 )
-	    source $ModDir'phpLDAPadmin.sh'
+	    source $ModDir'LDAPolc.sh'
 	    ;;
 	08 )
-	    source $ModDir'SSHD.sh'
+	    source $ModDir'phpLDAPadmin.sh'
 	    ;;
 	09 )
-	    source $ModDir'NX.sh'
+	    source $ModDir'SSHD.sh'
 	    ;;
 	10 )
-	    source $ModDir'DenyHosts.sh'
+	    source $ModDir'NX.sh'
 	    ;;
 	11 )
-	    source $ModDir'NTPD.sh'
+	    source $ModDir'DenyHosts.sh'
 	    ;;
 	12 )
-	    source $ModDir'Apache.sh'
+	    source $ModDir'NTPD.sh'
+	    ;;
+	13 )
+	    source $ModDir'Monitorix.sh'
+	    ;;
+	14 )
+	    source $ModDir'DNSMasq.sh'
+	    ;;
+	15 )
+	    source $ModDir'MRTG.sh'
+	    ;;
+	16 )
+	    source $ModDir'Nagios.sh'
+	    ;;
+	17 )
+	    source $ModDir'RADIUS.sh'
+	    ;;
+	18 )
+	    source $ModDir'RadSecProxy.sh'
+	    ;;
+	19 )
+	    source $ModDir'Firewall.sh'
+	    ;;
+	20 )
+	    source $ModDir'Conntrack.sh'
+	    ;;
+	21 )
+	    source $ModDir'Postfix.sh'
+	    ;;
+	22 )
+	    source $ModDir'PostgreSQL.sh'
+	    ;;
+	23 )
+	    source $ModDir'PostgreSQLUp.sh'
+	    ;;
+	24 )
+	    source $ModDir'JavaJDK.sh'
+	    ;;
+	25 )
+	    source $ModDir'NetDot.sh'
+	    ;;
+	26 )
+	    source $ModDir'JavaOracle.sh'
+	    ;;
+	27 )
+	    source $ModDir'JBossAS.sh'
 	    ;;
 	28 )
 	    source $ModDir'APImageGenerator.sh'
