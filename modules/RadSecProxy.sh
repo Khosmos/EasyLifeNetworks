8081a6d5de0997430670c5c689a5574bd46eb616
Cosme Corrêa <cosmefc@id.uff.br>
2015-11-23 11:00:01 -0500

    Fixing issue #21

--- modules/RadSecProxy.sh
+++ modules/RadSecProxy.sh
@@ -4,7 +4,7 @@
 # Configuration Tool for an Easy Life
 # Version 20151120
 #
-# Wiki module
+# RadSecProxy module
 #
 # Luiz Magalhaes
 # Cosme Faria Corrêa
@@ -14,9 +14,9 @@
 #set -xv
 
 #Show readme.txt, if it exists
-[ -e $ModDir'Wiki/Wiki-readme.txt' ] && DisplayMsg "Wiki" "`cat $ModDir'Wiki/Wiki-readme.txt'`"
+[ -e $ModDir'RadSecProxy/RadSecProxy-readme.txt' ] && DisplayMsg "RadSecProxy" "`cat $ModDir'RadSecProxy/RadSecProxy-readme.txt'`"
 
-DisplayYN "EasyLife Networks - Wiki" \
+DisplayYN "EasyLife Networks - RadSecProxy" \
 "This module will:
  1) Install Packets
  2) Compile radsec
@@ -28,10 +28,10 @@ DisplayYN "EasyLife Networks - Wiki" \
 yum install gcc openssl openssl-devel -y
 
 #2 Compile radsec
-cp $ModDir/Wiki/Wiki-1.6.5.tar.gz /tmp/
+cp $ModDir/RadSecProxy/RadSecProxy-1.6.5.tar.gz /tmp/
 cd /tmp
-tar -xvf Wiki-1.6.5.tar.gz
-cd Wiki-1.6.5
+tar -xvf RadSecProxy-1.6.5.tar.gz
+cd RadSecProxy-1.6.5
 ./configure && make && make install
 
 #3 Setup
@@ -39,24 +39,24 @@ cd Wiki-1.6.5
 now=`date +%Y%m%d-%H%M%S`
 mv /etc/raddb/clients.conf /etc/raddb/clients.conf.$now
 mv /etc/raddb/proxy.conf /etc/raddb/proxy.conf.$now
-mv /etc/Wiki.conf /etc/Wiki.conf.$now
+mv /etc/RadSecProxy.conf /etc/RadSecProxy.conf.$now
 # new confs
-cp -Rf $ModDir/Wiki/RNP/clients.conf /etc/raddb/clients.conf
-cp -Rf $ModDir/Wiki/RNP/proxy.conf /etc/raddb
-cp -Rf $ModDir/Wiki/RNP/Wiki.conf /etc
+cp -Rf $ModDir/RadSecProxy/RNP/clients.conf /etc/raddb/clients.conf
+cp -Rf $ModDir/RadSecProxy/RNP/proxy.conf /etc/raddb
+cp -Rf $ModDir/RadSecProxy/RNP/RadSecProxy.conf /etc
 #  Certs
-cp -Rf $ModDir/Wiki/RNP/*.crt /etc/raddb/certs
-cp -Rf $ModDir/Wiki/RNP/*.key /etc/raddb/certs
+cp -Rf $ModDir/RadSecProxy/RNP/*.crt /etc/raddb/certs
+cp -Rf $ModDir/RadSecProxy/RNP/*.key /etc/raddb/certs
 # EasyLifeNetworks net
-cat $ModDir/Wiki/EasyLifeNetworks.txt >> /etc/raddb/clients.conf
+cat $ModDir/RadSecProxy/EasyLifeNetworks.txt >> /etc/raddb/clients.conf
 sed -i s/RADIUSPASS/$RADIUSPASS/g /etc/raddb/clients.conf
 
 #4 Start
-cp $ModDir/Wiki/Wiki /etc/init.d/
-chmod 755 /etc/init.d/Wiki
-chkconfig Wiki on
-service Wiki restart
+cp $ModDir/RadSecProxy/RadSecProxy /etc/init.d/
+chmod 755 /etc/init.d/RadSecProxy
+chkconfig RadSecProxy on
+service RadSecProxy restart
 service radiusd restart
 
 #Show postinstall.txt, if it exists
-[ -e $ModDir'Wiki/Wiki-postinstall.txt' ] && DisplayMsg "Wiki" "`cat $ModDir'Wiki/Wiki-postinstall.txt'`" || ( echo 'Wiki module finished'; read )
+[ -e $ModDir'RadSecProxy/RadSecProxy-postinstall.txt' ] && DisplayMsg "RadSecProxy" "`cat $ModDir'RadSecProxy/RadSecProxy-postinstall.txt'`" || ( echo 'RadSecProxy module finished'; read )
---
+++ modules/RadSecProxy/RadSecProxy-readme.txt
@@ -0,0 +1,12 @@
+What is RadSecProxy?
+RadSecProxy provides a means to secure the communication 
+between RADIUS/TCP peers on the transport layer.
+
+Why RadSecProxy?
+-It is part of EDUROAM design.
+
+Status
+-Mature
+-It needs an RNP zip, or similar
+
+See RNP.txt before install.
