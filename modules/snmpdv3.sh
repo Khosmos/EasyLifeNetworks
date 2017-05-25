3398848c34b7e1dacd64ac61ca0c3cc06ea536f0
Ddehoul <ddehoul@gmail.com>
2017-05-24 22:25:41 -0300

    Update snmpdv3.sh

--- modules/snmpdv3.sh
+++ modules/snmpdv3.sh
@@ -9,18 +9,15 @@
 #
 # ...
 #set -xv
-
 # Show Actions
-DisplayYN "EasyLife Networks - SNMPDv3" \
+"EasyLife Networks - SNMPDv3" \
 "This module will:
  1) Install SNMPD
  2) Copy Templates
  3) Setup SNMPD
  4) Create V3 User
- 5) Start Processes
-
-" "Install" "Cancel" || exit
-
+ 5)Start Processes
+ " "Install" "Cancel" || exit
 #1 Install
 yum install net-snmp net-snmp-utils -y
 
@@ -34,13 +31,13 @@ sed -i s/LDAPPRIMARYUIDMAIL/$LDAPPRIMARYUIDMAIL/g /etc/snmp/snmpd.conf
 sed -i s/MACHINE/$MACHINE/g /etc/snmp/snmpd.conf
 sed -i s/SYSLOCATION/"$SYSLOCATION"/g /etc/snmp/snmpd.conf
 
-#4 Create a V3 Account
+#4 Create V3 User
 service snmpd stop
 echo $SNMPUSER | net-snmp-create-v3-user -a $SNMPAUTH -A $SNMPPASSWD -X $SNMPPRIVPASSWD -x $SNMPENCRYPTION
 
 #5 Start Processes
-service snmpd status
 service snmpd start
+service snmpd status
 
 # Show Postinstall.txt, if it exists
 [ -e $ModDir'SNMPDv3/SNMPDv3-postinstall.txt' ] && DisplayMsg "SNMPv3" "`cat $ModDir'SNMPDv3/SNMPDv3-postinstall.txt'`" || ( echo 'SNMPv3 module finished, press <enter> to exit'; read)
